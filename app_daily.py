a6b051014a08bb1917ace22428d48bd84de84cc7
Tim Munroe <munroe.tim@gmail.com>
2018-10-20 16:09:00 -0400

    Oct. 20

--- app_daily.py
+++ app_daily.py
@@ -1,6 +1,6 @@
 import pyperclip
 import sys
-import utils_daily
+import utils
 
 # print('Number of arguments:', len(sys.argv), 'arguments.')
 # print('Argument List:', str(sys.argv))
@@ -10,9 +10,10 @@ if len(sys.argv) > 1:
         site = 'record'
     elif (sys.argv)[1] == 'standard':
         site = 'standard'
-    else:
-        (sys.argv)[1] == 'examiner':
+    elif (sys.argv)[1] == 'examiner':
         site = 'examiner'
+    elif (sys.argv)[1] == 'star':
+        site = 'star'
 else:
     site = 'spectator'
 print("Site is: ", site)
@@ -20,35 +21,39 @@ print("Site is: ", site)
 # DYNAMIC VALUES
 files = {
     "spectator": {
-        "daily": 'Site-Stats-Over-Time-Oct-18-2018-thespec-com.csv',
-        "posts": 'Top-10-posts-by-total-engaged-minutes-Oct-18-2018-thespec-com-post.csv',
+        "daily": 'Site-Stats-Over-Time-Oct-19-2018-thespec-com.csv',
+        "posts": 'Top-10-posts-by-total-engaged-minutes-Oct-19-2018-thespec-com-post.csv',
         "ma": 'Site-Stats-Over-Time-Jul-01-2018-Sep-30-2018-thespec-com.csv'
     },
     "record": {
-        "daily": 'Site-Stats-Over-Time-Oct-18-2018-therecord-com.csv',
-        "posts": 'Top-10-posts-by-total-engaged-minutes-Oct-18-2018-therecord-com-post.csv',
+        "daily": 'Site-Stats-Over-Time-Oct-19-2018-therecord-com.csv',
+        "posts": 'Top-10-posts-by-total-engaged-minutes-Oct-19-2018-therecord-com-post.csv',
         "ma": 'Site-Stats-Over-Time-Jul-01-2018-Sep-30-2018-therecord-com.csv'
     },
     "standard": {
-        "daily": 'Site-Stats-Over-Time-Oct-18-2018-therecord-com.csv',
-        "posts": 'Top-10-posts-by-total-engaged-minutes-Oct-18-2018-therecord-com-post.csv',
-        "ma": 'Site-Stats-Over-Time-Jul-01-2018-Sep-30-2018-therecord-com.csv'
+        "daily": 'Site-Stats-Over-Time-Oct-19-2018-stcatharinesstandard-ca.csv',
+        "posts": 'Top-10-posts-by-total-engaged-minutes-Oct-19-2018-stcatharinesstandard-ca.csv',
+        "ma": 'Site-Stats-Over-Time-Jul-01-2018-Sep-30-2018-stcatharinesstandard-ca.csv'
     },
     "examiner": {
-        "daily": 'Site-Stats-Over-Time-Oct-18-2018-therecord-com.csv',
-        "posts": 'Top-10-posts-by-total-engaged-minutes-Oct-18-2018-therecord-com-post.csv',
-        "ma": 'Site-Stats-Over-Time-Jul-01-2018-Sep-30-2018-therecord-com.csv'
+        "daily": 'Site-Stats-Over-Time-Oct-19-2018-thepeterboroughexaminer-com.csv',
+        "posts": 'Top-10-posts-by-total-engaged-minutes-Oct-19-2018-thepeterboroughexaminer-com-post.csv',
+        "ma": 'Site-Stats-Over-Time-Jul-01-2018-Sep-30-2018-thepeterboroughexaminer-com.csv'
     },
-    
+    "star": {
+        "daily": 'Site-Stats-Over-Time-Oct-19-2018-thestar-com.csv',
+        "posts": 'Top-10-posts-by-total-engaged-minutes-Oct-19-2018-thestar-com.csv',
+        "ma": 'Site-Stats-Over-Time-Jul-01-2018-Sep-30-2018-thestar-com.csv'
+    },
 }
 
-daily_file_path = site + '/daily/' + files[site]['daily']
-posts_file_path = site + '/daily/' + files[site]['posts']
-ma_file_path = site + '/3_month_avg/' + files[site]['ma']
+daily_file_path = 'reports/daily/' + files[site]['daily']
+posts_file_path = 'reports/daily/' + files[site]['posts']
+ma_file_path = 'reports/3_month_avg/' + files[site]['ma']
 ma_days = 92
 
 # search daily folder, find latest 'stats over time', then look for matching 'top 10 posts'.
-# bob = utils_daily.newest('spec/daily/')
+# bob = utils.newest('spec/daily/')
 # print(bob)
 
 # STATIC VALUES
@@ -57,58 +62,58 @@ sngl_line = '-----------------------------------------------------------\n'
 nl = '\n'
 
 # MAIN
-daily_values = utils_daily.return_csv(daily_file_path)
-ma_values = utils_daily.return_csv(ma_file_path)
-posts_values = utils_daily.return_csv(posts_file_path)
-data = utils_daily.site_stats(daily_values, ma_values, ma_days)
+daily_values = utils.return_csv(daily_file_path)
+ma_values = utils.return_csv(ma_file_path)
+posts_values = utils.return_csv(posts_file_path)
+data = utils.site_stats(daily_values, ma_values, ma_days)
 
-# posts = utils_daily.post_stats()
+# posts = utils.post_stats()
 s = ''
 s += f'Daily web report: {site.title()} for {daily_values["Date"]}' + nl + nl
 s += dbl_line
 s += "TOP POSTS: by Total Engaged Minutes" + nl + sngl_line
 
 for rank, item in enumerate(posts_values[:5], start=1):
-    s += f"{rank}. {item['Title'].title()}\nBy {item['Authors'].title()} in {item['Section']}" + nl + nl
+    s += f"{rank}. {item['Title'].title().replace('’T','’t').replace('’S', '’s').replace('’M','’m').replace('’R','’r')}\nBy {item['Authors'].title()} in {item['Section']}" + nl + nl
     s += f"VISITORS: {str(round(float(item['Sort (Engaged minutes)'])/float(item['Visitors']),2))} min/visitor, "
-    s += f"visitors: {utils_daily.humanize_number(item['Visitors'],1)}, returning: {(utils_daily.percentage(item['Returning vis.'], item['Visitors']))}%" + nl
-    s += f"TRAFFIC %: social {(utils_daily.percentage(item['Social refs'], item['Views']))}, "
-    s += f"search&other {utils_daily.percentage(utils_daily.sum_safe([ item['Search refs'], item['Other refs'] ]), item['Views'])}, "
-    s += f"internal {(utils_daily.percentage(item['Internal refs'], item['Views']))}" + nl
-    s += f"DEVICES %: mobile {(utils_daily.percentage(item['Mobile views'], item['Views']))}, desktop {(utils_daily.percentage(item['Desktop views'], item['Views']))}, "
-    s += f"tablet {(utils_daily.percentage(item['Tablet views'], item['Views']))}"
+    s += f"visitors: {utils.humanize_number(item['Visitors'],1)}, returning: {(utils.percentage(item['Returning vis.'], item['Visitors']))}%" + nl
+    s += f"TRAFFIC %: social {(utils.percentage(item['Social refs'], item['Views']))}, "
+    s += f"search&other {utils.percentage(utils.sum_safe([ item['Search refs'], item['Other refs'] ]), item['Views'])}, "
+    s += f"internal {(utils.percentage(item['Internal refs'], item['Views']))}" + nl
+    s += f"DEVICES %: mobile {(utils.percentage(item['Mobile views'], item['Views']))}, desktop {(utils.percentage(item['Desktop views'], item['Views']))}, "
+    s += f"tablet {(utils.percentage(item['Tablet views'], item['Views']))}"
     s += nl + nl + sngl_line
 
-s += nl 
+s += nl
 s += dbl_line
 s += f'''SITE         Totals | Diff. | KPIs
 DETAILS:            | vs MA |
 '''
 s += sngl_line
-s += f"{'Post views'.ljust(10)}  {utils_daily.humanize_number(data['postv']['new'],0).rjust(6)} "
+s += f"{'Post views'.ljust(10)}  {utils.humanize_number(data['postv']['new'],0).rjust(6)} "
 s += f"{data['postv']['delta'].rjust(8)}   {data['postv']['kpi_new']} views/vis." + nl
-s += f"{'Visitors'.ljust(10)}  {utils_daily.humanize_number(data['visitors']['new'],0).rjust(6)}    --------------------------------------" + nl
-s += f"{'Minutes'.ljust(10)}  {utils_daily.humanize_number(data['minutes']['new'],0).rjust(6)} "
+s += f"{'Visitors'.ljust(10)}  {utils.humanize_number(data['visitors']['new'],0).rjust(6)}    --------------------------------------" + nl
+s += f"{'Minutes'.ljust(10)}  {utils.humanize_number(data['minutes']['new'],0).rjust(6)} "
 s += f"{data['minutes']['delta'].rjust(8)}   {data['minutes']['kpi_new']} minutes/vis." + nl
 s += dbl_line
 s += f"TRAFFIC DETAILS:  % of views  |  VISITORS %:" + nl
 s += f"----------------------------- |  New {data['visitor_type']['new']}, Returning {data['visitor_type']['returning']}" + nl
-s += f"{'Search + Others'.ljust(16)} {(utils_daily.percentage(data['traffic']['s+o'], data['postv']['new'])).rjust(6)}"
+s += f"{'Search + Others'.ljust(16)} {(utils.percentage(data['traffic']['s+o'], data['postv']['new'])).rjust(6)}"
 s += f"       |  --------------------------" + nl
-s += f"{'Internal'.ljust(16)} {(utils_daily.percentage(data['traffic']['internal'], data['postv']['new'])).rjust(6)}"
+s += f"{'Internal'.ljust(16)} {(utils.percentage(data['traffic']['internal'], data['postv']['new'])).rjust(6)}"
 s += f"       |  DEVICES %:" + nl
-s += f"{'Direct'.ljust(16)} {(utils_daily.percentage(data['traffic']['direct'], data['postv']['new'])).rjust(6)}"
+s += f"{'Direct'.ljust(16)} {(utils.percentage(data['traffic']['direct'], data['postv']['new'])).rjust(6)}"
 s += f"       |  Mobile {data['devices']['mobile']}" + nl
-s += f"{'Facebook'.ljust(16)} {(utils_daily.percentage(data['traffic']['fb'], data['postv']['new'])).rjust(6)}"
+s += f"{'Facebook'.ljust(16)} {(utils.percentage(data['traffic']['fb'], data['postv']['new'])).rjust(6)}"
 s += f"       |  Desktop {data['devices']['desktop']}" + nl
-s += f"{'Twitter'.ljust(16)} {(utils_daily.percentage(data['traffic']['tco'], data['postv']['new'])).rjust(6)}"
+s += f"{'Twitter'.ljust(16)} {(utils.percentage(data['traffic']['tco'], data['postv']['new'])).rjust(6)}"
 s += f"       |  Tablet {data['devices']['tablet']}" + nl
 s += sngl_line
 s += f"Notes: MA = 3-month moving average" + nl
 s += f"Due to rounding, numbers may not add up to 100%" + nl
 s += f"Google search accounts for nearly all 'Search' views.\nGoogle News and Google APIs account for most 'Other' views." + nl
 s += f"Based on *post* views"
-s += f", about {utils_daily.percentage(data['postv']['new'], data['pagev']['new'])}% of day's page views" + nl + dbl_line
+s += f", about {utils.percentage(data['postv']['new'], data['pagev']['new'])}% of day's page views" + nl + dbl_line
 
 # print(s)
 # pprint.pprint(posts_values)
---
+++ utils.py
@@ -0,0 +1,139 @@
+import csv
+import os
+
+
+def site_stats(new, ma, units):
+    result = {
+        "pagev": {
+            "new": new['Views (all pages)'],
+            "avg": sum_csv_values(ma, 'Views (all pages)'),
+            "delta": ''
+        },
+        "postv": {
+            "new": new['Views (posts)'],
+            "avg": sum_csv_values(ma, 'Views (posts)'),
+            "delta": '',
+            "kpi_new": '',
+        },
+        "visitors": {
+            "new": new['Visitors (posts)'],
+            "avg": sum_csv_values(ma, 'Visitors (posts)'),
+            "delta": '',
+        },
+        "minutes": {
+            "new": new['Engaged Minutes (posts)'],
+            "avg": sum_csv_values(ma, 'Engaged Minutes (posts)'),
+            "delta": '',
+            "kpi_new": '',
+        },
+        "visitor_type": {
+            "new": percentage(new['New vis.'], new['Visitors (posts)']),
+            "returning": percentage(new['Returning vis.'], new['Visitors (posts)'])
+        },
+        "devices": {
+            "mobile": new['Mobile views'],
+            "desktop": new['Desktop views'],
+            "tablet": new['Tablet views']
+        },
+        "traffic": {
+            "s+o": str(float(new['Search refs']) + float(new['Other refs'])),
+            "internal": new['Internal refs'],
+            "direct": new['Direct refs'],
+            "fb": new['Fb refs'],
+            "tco": new['Tw refs']
+        }
+    }
+
+    result['pagev']['delta'] = vs_ma(result['pagev']['new'], result['pagev']['avg'], units)
+    result['postv']['delta'] = vs_ma(result['postv']['new'], result['postv']['avg'], units)
+    result['visitors']['delta'] = vs_ma(result['visitors']['new'], result['visitors']['avg'], units)
+    result['minutes']['delta'] = vs_ma(result['minutes']['new'], result['minutes']['avg'], units)
+    result['devices']['mobile'] = percentage(result['devices']['mobile'], result['postv']['new'])
+    result['devices']['desktop'] = percentage(result['devices']['desktop'], result['postv']['new'])
+    result['devices']['tablet'] = percentage(result['devices']['tablet'], result['postv']['new'])
+    result['postv']['kpi_new'] = str(round((float(result['postv']['new'])/float(result['visitors']['new'])), 2))
+    result['minutes']['kpi_new'] = str(round((float(result['minutes']['new'])/float(result['visitors']['new'])), 2))
+    return result
+
+
+def newest(folder):
+    cur_path = os.path.dirname(__file__)
+    folder_path = os.path.relpath(folder, cur_path)
+    files = os.listdir(folder_path)
+    paths = [os.path.join(folder_path, basename) for basename in files if basename.endswith('.csv')]
+    return max(paths, key=os.path.getctime)
+
+
+def return_csv(file_path):
+    cur_path = os.path.dirname(__file__)
+    file = os.path.relpath(file_path, cur_path)
+    with open(file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        result = []
+        for row in reader:
+            result.append(dict(row))
+        if len(result) == 1:
+            return result[0]
+        else:
+            return result
+
+
+def sum_csv_values(d, key):
+    # get and sum values from CSV (converted to dict) given a key
+    divisor = len(d)
+    # print("Divisor is: ", divisor)
+    # assume all values are string represenations of integers, though some end in '.0'
+    # values that are string reps of floats we aren't dealing with at moment
+    result = [int(item[key].replace('.0', '')) for item in d]
+    theSum = sum(result)
+    return str(theSum)
+
+
+def sum_safe(l):
+    #  handles if item is no value in list
+    new_list = []
+    for item in l:
+        if not item:
+            item = '0'
+        item = float(item)
+        new_list.append(item)
+    return sum(new_list)
+
+
+def vs_ma(new, avg, days):
+    daily_avg = float(avg)/days
+    # print("New value: ", humanize_number(new))
+    # print("Avg value: ", humanize_number(daily_avg, 0))
+    result = ((float(new) - daily_avg)/daily_avg)*100
+    result = f"{str(round(result, 1))}"
+    return result
+
+
+def humanize_number(value, fraction_point=1):
+    powers = [10 ** x for x in (12, 9, 6, 3, 0)]
+    human_powers = ('T', 'B', 'M', 'K', '')
+    is_negative = False
+    if not isinstance(value, float):
+        value = float(value)
+    if value < 0:
+        is_negative = True
+        value = abs(value)
+    for i, p in enumerate(powers):
+        if value >= p:
+            return_value = str(round(value / (p / (10.0 ** fraction_point))) /
+                               (10 ** fraction_point)) + human_powers[i]
+            break
+    if is_negative:
+        return_value = "-" + return_value
+    # remove pesky situation where xXX.0 occurs
+    return_value = return_value.replace('.0', '')
+    return return_value
+
+
+def percentage(part, sum):
+    # have to take into account part might be none (actually '')
+    if part is not '':
+        result = str(round((float(part)/float(sum)) * 100, 0)).replace('.0', '')
+        return result
+    else:
+        return '0'
